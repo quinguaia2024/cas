0ef568ab65b67e524a5a8136a0042479a532894f
quinguaia2024 <quinguaia2024@gmail.com>
2025-10-07 11:36:13 +0100

    Firts version of Chris' portofolio

---
+++ src/content_option.js
@@ -0,0 +1,156 @@
+const logotext = "CHRIS";
+const meta = {
+    title: "Cristiano Fandula",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Cristiano Fandula",
+    animated: {
+        first: "They say I am a developer.",
+        second: "I prefer to say that I translate logic into digital emotion.",
+        third: "I don’t follow trends — I build them.",
+    },
+    description: "I work at the intersection of artificial intelligence, automation, and web development.",
+    description2: "Each project is an artifact. Each system, an idea that refused to be just theory.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Creating the invisible that moves the digital.",
+    aboutme: "I didn’t start with code.I started with curiosity.I always wanted to understand how the invisible takes shape,and I ended up discovering that, sometimes,logic is just a language to translate intuition.Today, I am a digital creator who walks between calculation and chaos.",
+    aboutme2: "I work with web development, automation, and artificial intelligence,building systems that not only function — they learn, adapt, and respond.",
+    aboutme3: "I don’t write just code.I write digital movements.And in the silence between one line and another,that is where the future tends to whisper first.",
+
+
+};
+const worktimeline = [{
+    jobtitle: "Software Developer",
+    where: "Infosi",
+    date: "2024",
+},
+{
+    jobtitle: "Senior Intern",
+    where: "Instituto de Telecomunicações | ITEL",
+    date: "2023",
+},
+{
+    jobtitle: "IT Technician",
+    where: "Instituto de Telecomunicações | ITEL",
+    date: "2023",
+},
+];
+
+const skills = [{
+    name: "Python (Flask)",
+    value: 90,
+},
+{
+    name: "AI Instant Agents",
+    value: 85,
+},
+{
+    name: "Laravel (PHP",
+    value: 85,
+},
+{
+    name: "MySQL / PostgreSQL / SQLite",
+    value: 75,
+},
+{
+    name: "Apache / XAMPP / Composer / Virtualenv",
+    value: 82,
+},
+{
+    name: "API Integration & Auth Systems",
+    value: 80,
+},
+{
+    name: "Google Cloud / Integrations",
+    value: 85,
+},
+{
+    name: "n8n",
+    value: 60,
+},
+{
+    name: "Software installation and configuration",
+    value: 60,
+},
+{
+    name: "Diagnostics and maintenance of systems",
+    value: 60,
+},
+];
+
+const services = [{
+    title: "Web Development",
+    description: "What the eye sees is only the surface.What is real happens between the lines — where logic meets aesthetics.",
+    description2: "I build systems that breathe.Pages that think.Each interface is a code dressed as art.",
+},
+{
+    title: "Automation & Low-Code",
+    description: "Automation isn't about laziness.It's about awareness.I teach machines to perceive,to decide,to act.",
+    description2: "The result? Order in the digital chaos.",
+},
+{
+    title: "IT & Technical Support",
+    description: "Not every failure is digital.Sometimes, the noise comes from the physical world.Among cables, servers, and command lines,I maintain the balance between the real and the binary.",
+    description2: "Because even machines need someone to understand them.",
+},
+];
+
+const dataportfolio = [
+    {
+        img: "https://github.com/ChristianoAlpha/ChristianoAlpha/blob/main/car.png?raw=true",
+        description: "AngoNews",
+        description2: "Real-time information, clean interface, and smooth navigation.Where every news item is a living fragment of the now.",
+        link: "#",
+    },
+    {
+        img: "https://github.com/ChristianoAlpha/ChristianoAlpha/blob/main/news.png?raw=true",
+        description: "AngoCar",
+        description2: "The platform allows users to explore, filter, and book cars with precision, fluidity, and safety..",
+        link: "#",
+    },
+    {
+        img: "https://github.com/ChristianoAlpha/ChristianoAlpha/blob/main/venus.png?raw=true",
+        description: "VenusDigital",
+        description2: "We create visual experiences that inspire, communicate, and transform. Where image meets purpose — and the brand gains soul.",
+        link: "#",
+    },
+
+
+
+
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "cristianofandula07@gmail.com",
+    YOUR_FONE: "(244)924-108-223",
+    description: "Do you want to understand what comes after logic? Get in touch or Explore my projects",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_o12llm7",
+    YOUR_TEMPLATE_ID: "template_w11ah6t",
+    YOUR_USER_ID: "EbilYLIQdO7JPWpmS",
+};
+
+const socialprofils = {
+    github: "https://github.com/ChristianoAlpha",
+    linkedin: "https://www.linkedin.com/in/cristiano-fandula/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
